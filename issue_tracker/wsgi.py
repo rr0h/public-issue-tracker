149db49b3325a890449d8706f5a4e159501ca425
Rohit <119391810+rr0h@users.noreply.github.com>
2025-12-03 22:10:51 +0530

    Add WSGI configuration

---
+++ issue_tracker/wsgi.py
@@ -0,0 +1,11 @@
+"""
+WSGI config for issue_tracker project.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'issue_tracker.settings')
+
+application = get_wsgi_application()
