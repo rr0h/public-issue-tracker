65cb1a60fb69926a722f7fed69a2ddae230bf681
Rohit <119391810+rr0h@users.noreply.github.com>
2025-12-03 22:10:50 +0530

    Add main URL configuration

---
+++ issue_tracker/urls.py
@@ -0,0 +1,20 @@
+"""
+URL configuration for issue_tracker project.
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('issues.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('api/', include('issues.api_urls')),
+]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
